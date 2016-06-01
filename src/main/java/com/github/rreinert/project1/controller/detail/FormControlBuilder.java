cdfa5bdba9de7ef627629c6e31b35f8b982e5c84
gitproject1 <gitproject1@gmail.com>
2016-06-01 12:58:22 +0000

    creating repository

--- src/main/java/com/github/rreinert/project1/controller/detail/FormControlBuilder.java
+++
@@ -1,7 +0,0 @@
-package com.github.rreinert.project1.controller.detail;
-
-import org.primefaces.extensions.model.dynaform.DynaFormRow;
-
-public interface FormControlBuilder {
-	public void populateRow(DynaFormRow row);
-}
